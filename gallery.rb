0e5810ee0612a82324d3d4b1ea886571219e1d5b
jtoy <jtoy@jtoy.net>
2017-01-07 12:23:22 -0500

    Update gallery.rb

--- gallery.rb
+++ gallery.rb
@@ -3,7 +3,7 @@ to_check = Dir.pwd
 dirs = Dir.entries(to_check).select {|entry| puts entry; File.directory? File.join(to_check,entry) and !(entry[0] =='.') }
 loop do
   dirs.each do |dir|
-    cmd = " timeout 20 processing-java --sketch=#{File.join(to_check,dir)} --run"
+    cmd = " timeout 60 processing-java --sketch=#{File.join(to_check,dir)} --run"
     puts cmd
     `#{cmd}`
   end
